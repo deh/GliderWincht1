73fd025788e3ec7800212f13edebd4009e948ebe
Troy Cosentino <troycosentnio@gmail.com>
2014-04-26 19:12:04 -0700

    basic build and run scripts.. the two files

---
+++ src/MasterController.java
@@ -0,0 +1,9 @@
+
+
+public class MasterController {
+
+    public static void main(String args[]) {
+        System.out.println("MasterController!");
+    }
+
+}
---
+++ src/Physics.java
@@ -0,0 +1,9 @@
+
+
+public class Physics {
+
+    public static void main(String args[]) {
+        System.out.println("Physics");
+    }
+
+}
